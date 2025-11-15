00960a6962d7a96070d18b00752313dfd18edd8b
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-11-15 16:43:41 +0530

    fg

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3001", // or your frontend domain
+            value: "https://lolify.vercel.app", // or your frontend domain
           },
           {
             key: "Access-Control-Allow-Methods",
